706b1daa64a2951b4746af7906a1ea3deed46b0c
TakaGui <gui.yukio175@gmail.com>
2020-09-20 21:40:18 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '3.6.2',
+      version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false,
     instance: {
       dbName: 'jest'
-    }
+    },
+    autoStart: false
   }
 }
